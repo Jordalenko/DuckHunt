7fbf64f4b1b69b596ba48a7e8ed3c55156c779c3
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-14 17:48:53 +0000

    Add Bullet and Score JS and CSS

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ let bullets = 5;
 let count = 0;
 
 function shoot(bird) {
+    bullets--;
+    if (bullets >= 0) {
     bird.style.display = "none";
+    document.getElementById("score").innerHTML = ++count;
+    } else if (bullets == -1) {
+            alert("Reload Page");
+        }
+    }
 
-    document.getElementById("score").innerHTML = "Score:" + ++count;
-}
+    window.onclick = function() {
+        if(bullets > 0) {
+            bullets--;
+            document.getElementById("bullets").innerHTML = bullets;
+        }
+    }
