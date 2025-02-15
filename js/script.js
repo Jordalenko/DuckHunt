2f2ab668189e1c5483f630a8f7a8d9e74bff081c
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-15 16:49:41 +0000

    correct bullet minus js.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ let bullets = 5;
 let count = 0;
 
 function shoot(bird) {
-    bullets--;
-    if (bullets >= 0) {
+    if (bullets > 0) {
     bird.style.display = "none";
     document.getElementById("score").innerHTML = ++count;
     } else if (bullets == -1) {
@@ -12,8 +11,8 @@ function shoot(bird) {
     }
 
     window.onclick = function() {
-        if(bullets > 0) {
-            bullets--;
-            document.getElementById("bullets").innerHTML = bullets;
+        bullets--;
+        if(bullets >= 0) {
+                document.getElementById("bullets").innerHTML = bullets;
         }
     }
