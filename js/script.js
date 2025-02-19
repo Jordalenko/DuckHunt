9e49da0138a17d5a89994f5fa16e41cb59599938
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-19 19:06:18 +0000

    Create game over page.

--- js/script.js
+++ js/script.js
@@ -3,27 +3,30 @@ let count = 0;
 
 function shoot(bird) {
     if (bullets > 0) {
-    bird.style.display = "none";
-    document.getElementById("score").innerHTML = `Score: ${++count}`;
-    } else if (bullets == -1) {
-            alert("Reload Page");
-        }
+        bird.style.display = "none";
+        document.getElementById("score").innerHTML = `Score: ${++count}`;
     }
+}
 
     window.onclick = function(e) {
         bullets--;
-        if(bullets >= 0) {
+        if (bullets >= 0) {
                 document.getElementById("bullets").innerHTML = `Bullets: ${bullets}`;
                 if(e.target.nodeName == "IMG") {
                     document.getElementById("hit").play();
                 } else {
                     document.getElementById("miss").play();
                 }
-        } 
+        } else if (bullets == -1) {
+            setTimeout(GameOver, 2000)
+        }
     }
 
+function GameOver() {
+    window.open("./game-over.html", "_self")
+    }
 
-function GamePlay () {
+function GamePlay() {
     window.open("./game.html", "_self");
 }
 
