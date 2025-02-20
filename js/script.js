7208d558cc2574ca9aae155adb2298638659691e
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-20 21:59:42 +0000

    Set temp score and bullet counters to confirm syntax.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let bullets = 8;
 let count = 0;
 
+function scoring() {
+    document.getElementById("currentScore").innerHTML = `Score: ${++count}`;
+}
+
 function shoot(bird) {
     if (bullets > 0) {
+        scoring()
         bird.style.display = "none";
         document.getElementById("hit").play();
     }
@@ -11,16 +16,15 @@ function shoot(bird) {
     window.onclick = function(e) {
         bullets--;
         if (bullets >= 0) {
-            if(e.target.id == "b1, b2, b3, b4, b5, b6, b7, b8") {
-                document.getElementById("score") = console.log(`Score: ${++count}`);
-            } else {
+            document.getElementById("currentBullets").innerHTML = `Bullets: ${bullets}`;
+        if(e.target.class !== "bird") {
                 document.getElementById("miss").play();
-                document.getElementById("quack").play();
             }
         } else if (bullets == -1) {
-            setTimeout(GameOver, 2000)
+                setTimeout(GameOver, 2000)
         }
     }
+ 
 
 function GameOver() {
     window.open("./game-over.html", "_self")
