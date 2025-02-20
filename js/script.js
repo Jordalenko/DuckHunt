f0b3f8199446a67a991a1ecc65e16f47c5517537
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-20 16:35:05 +0000

    Add responsive sizing for mobile and cleaned up code.

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ let count = 0;
 function shoot(bird) {
     if (bullets > 0) {
         bird.style.display = "none";
-        document.getElementById("score").innerHTML = `Score: ${++count}`;
+        document.getElementById("hit").play();
     }
 }
 
     window.onclick = function(e) {
         bullets--;
         if (bullets >= 0) {
-                document.getElementById("bullets").innerHTML = `Bullets: ${bullets}`;
-                if(e.target.nodeName == "IMG") {
-                    document.getElementById("hit").play();
-                } else {
-                    document.getElementById("miss").play();
-                }
+            if(e.target.id == "b1, b2, b3, b4, b5, b6, b7, b8") {
+                document.getElementById("score") = console.log(`Score: ${++count}`);
+            } else {
+                document.getElementById("miss").play();
+                document.getElementById("quack").play();
+            }
         } else if (bullets == -1) {
             setTimeout(GameOver, 2000)
         }
