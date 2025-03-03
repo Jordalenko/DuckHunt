3e7e704f60149990191f892d4f0df4cf7c9d74e9
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-03-03 21:30:09 +0000

    Migrate onclick functions from html to js with event listeners.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,53 @@
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+const b5 = document.getElementById("b5");
+const b6 = document.getElementById("b6");
+const b7 = document.getElementById("b7");
+const b8 = document.getElementById("b8");
+const currentScoreDisplay = document.getElementById("currentScore");
+const startGame = document.getElementById("startButton");
+const restartGame = document.getElementById("gameEnd");
+const bulletCounter = document.getElementById("currentBullets");
+
+startGame.addEventListener("click", gameStart);
+restartGame.addEventListener("click", gameRestart);
+b1.addEventListener("click", shoot);
+b2.addEventListener("click", shoot);
+b3.addEventListener("click", shoot);
+b4.addEventListener("click", shoot);
+b5.addEventListener("click", shoot);
+b6.addEventListener("click", shoot);
+b7.addEventListener("click", shoot);
+b8.addEventListener("click", shoot);
+
 /* variable starting values */
 
 let bullets = 8;
 let count = 0;
 
-const startGame = document.getElementById("startButton");
+/* game play - hit birds */
 
-startGame.addEventListener("click", gameStart);
+function shoot(e) {
+    console.log("bird");
+    if (e.target.class == "bird") {
+        scoring();
+        bird.style.display = "none";
+        document.getElementById("hit").play();
+        setTimeout(() => {
+            gameOver();
+          }, "9000");
+        } if (bullets > 0) {
+            bullets--;
+    }
+}
 
-const restartGame = document.getElementById("gameEnd");
+/* attempt 1 */
 
-restartGame.addEventListener("click", gameRestart);
-
-/* game play shoot birds */
-
-function shoot(event) {
+/* function shoot(event) {
     const bird = event.currentTarget;
-    console.log(bird);
+    console.log("bird");
     if (bullets > 0) {
         scoring();
         bird.style.display = "none";
@@ -24,14 +56,14 @@ function shoot(event) {
             gameOver();
           }, "9000");
     }
-}
+} */
           
-/* game play miss birds */
+/* game play - miss birds */
 
 window.onclick = function(e) {
     if (bullets > 0) {
        bullets--;
-       document.getElementById("currentBullets").innerHTML = bullets;
+       bulletCounter.innerHTML = bullets;
         }
         
     if(e.target.class !== "bird") {
@@ -44,48 +76,17 @@ window.onclick = function(e) {
                 setTimeout(gameOver, 2000);
         }
 }
-
-
-const b1 = document.getElementById("b1");
-
-b1.addEventListener("click", shoot);
-
-const b2 = document.getElementById("b2");
-
-b2.addEventListener("click", shoot);
-
-const b3 = document.getElementById("b3");
-
-b3.addEventListener("click", shoot);
-
-const b4 = document.getElementById("b4");
-
-b4.addEventListener("click", shoot);
-
-const b5 = document.getElementById("b5");
-
-b5.addEventListener("click", shoot);
-
-const b6 = document.getElementById("b6");
-
-b6.addEventListener("click", shoot);
-
-const b7 = document.getElementById("b7");
-
-b7.addEventListener("click", shoot);
-
-const b8 = document.getElementById("b8");
-
-b8.addEventListener("click", shoot);
           
 /* game scoring */
           
 function scoring() {
-    document.getElementById("currentScore").innerHTML = ++count;
+    count += 1;
+    currrentScoreDisplay.innerHTML = count;
 }
           
 function missScoring() {
-    document.getElementById("currentScore").innerHTML = --count;
+    count -= 1;
+    currrentScoreDisplay.innerHTML = count;
 }
 
 
